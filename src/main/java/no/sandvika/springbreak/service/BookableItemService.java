e4f9575f92e236096446aa239814852efca91020
eirikholst <eirik.holst@bouvet.no>
2019-11-11 09:52:30 +0100

    dummy service init commit

--- src/main/java/no/sandvika/springbreak/service/BookableItemService.java
+++ src/main/java/no/sandvika/springbreak/service/BookableItemService.java
@@ -1,12 +1,35 @@
 package no.sandvika.springbreak.service;
 
-import jdk.jshell.spi.ExecutionControl;
 import no.sandvika.springbreak.domain.BookableItem;
+import no.sandvika.springbreak.domain.ItemLocation;
+import no.sandvika.springbreak.domain.ItemResource;
+import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class BookableItemService {
-    public List<BookableItem> getAllBookableItems() throws ExecutionControl.NotImplementedException {
-        throw new ExecutionControl.NotImplementedException("ÆÆÆÆ");
+    public List<BookableItem> getAllBookableItems() {
+        BookableItem printer = createItem("Printer", "Sandvika");
+        addResourceToItem("Grønt blekk", 1.0, printer);
+        addResourceToItem("Blått blekk", 2.0, printer);
+
+        BookableItem partyLion = createItem("Partyløven", "Sandvika");
+        addResourceToItem("BatteriProsent", 100.0, partyLion);
+
+        return Arrays.asList(printer, partyLion);
+    }
+
+    private BookableItem createItem(String name, String location) {
+        ItemLocation minLocation = new ItemLocation(location);
+        BookableItem bookableItem = new BookableItem(name);
+        bookableItem.setItemLocation(minLocation);
+        return bookableItem;
+    }
+
+    private void addResourceToItem(String resourceName, Double amount, BookableItem bookableItem) {
+        ItemResource itemResource = new ItemResource(resourceName, amount, bookableItem);
+        bookableItem.addResource(itemResource);
     }
 }
