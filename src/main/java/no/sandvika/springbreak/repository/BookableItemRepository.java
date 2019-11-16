1f0683d88d88cc06fbd64ce0e2676f351250089a
Knut Erik Skare <knut.erik.skare@gmail.com>
2019-11-16 16:01:19 +0100

    Fjernet litt stuff

--- src/main/java/no/sandvika/springbreak/repository/BookableItemRepository.java
+++ src/main/java/no/sandvika/springbreak/repository/BookableItemRepository.java
@@ -1,9 +1,6 @@
 package no.sandvika.springbreak.repository;
 
 import no.sandvika.springbreak.domain.BookableItem;
-import no.sandvika.springbreak.domain.ItemLocation;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
