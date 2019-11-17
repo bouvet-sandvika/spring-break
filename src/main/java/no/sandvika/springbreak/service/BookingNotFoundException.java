ace7884bf7c122a39c1990cc952b9c967faae9ce
eah <eirik.aasved.holst@spk.no>
2019-11-17 18:13:34 +0100

    cleanup

--- src/main/java/no/sandvika/springbreak/service/BookingNotFoundException.java
+++ src/main/java/no/sandvika/springbreak/service/BookingNotFoundException.java
@@ -1,7 +1,4 @@
 package no.sandvika.springbreak.service;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class BookingNotFoundException extends Exception {
 }
